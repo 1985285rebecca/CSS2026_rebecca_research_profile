a8ee23d9447626cc3df08c3d1d79fff2f46c7fef
1985285rebecca <1985285@students.wits.ac.za>
2026-02-02 23:18:02 +0200

    app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ if uploaded_file:
 # Changed to check for 'Year' since the header is 'Trends'
     if "Advert Year" in publications.columns:
         year_counts = publications["Advert Year"].value_counts().sort_index()
-        st.bar_chart(Advert Year_counts)
+        st.bar_chart(year_counts)
     else:
         st.write("The CSV does not have a 'Year' column to visualize trends.")
 
@@ -57,6 +57,7 @@ st.header("Publication Trends")
 st.header("Contact Information")
 email = "rebecca.setino@gmail.com"
 st.write(f"You can reach {name} at {email}.")
+
 
 
 
